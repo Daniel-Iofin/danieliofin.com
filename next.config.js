710fd43b28d4074c1e146f1a6428135c7d4bb59f
Daniel-Iofin <130488723+Daniel-Iofin@users.noreply.github.com>
2025-07-11 09:00:21 -0400

    Fix assets?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/danieliofin.com', // Adjust to match your GitHub Pages repo
+  basePath: '/danieliofin.com',
   assetPrefix: '/danieliofin.com/',
 };
 
